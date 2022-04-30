1f15ecc2d64e07f28133f0748f87aa36ae78c829
Krishna Rukmini Puthucode <krishnarukmini13@gmail.com>
2022-04-30 14:11:05 -0400

    Update partc.py

--- partc.py
+++ partc.py
@@ -1,55 +1,121 @@
 import cv2
 import depthai as dai
+import numpy as np
 
-# create a pipeline
-pipeline = dai.Pipeline()
 
-# define the source and output
-camRgb = pipeline.create(dai.node.ColorCamera)
-xoutVideo = pipeline.create(dai.node.XLinkOut)
+def getFrame(queue):
+    # Get the frame from the queue
+    frame = queue.get()
+    # convert the frame to OpenCV format and return
+    return frame.getCvFrame()
 
-xoutVideo.setStreamName("video")
 
-# properties
+def getMonoCamera(pipeline, isLeft):
+    # configure mono camera
+    mono = pipeline.createMonoCamera()
 
-camRgb.setBoardSocket(dai.CameraBoardSocket.RGB)
-camRgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
-camRgb.setVideoSize(1920, 1080)
-camRgb.setVideoSize(1280, 720)
+    # Set the camera resolution
+    mono.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
 
-xoutVideo.input.setBlocking(False)
-xoutVideo.input.setQueueSize(1)
+    if isLeft:
+        # Get left camera
+        mono.setBoardSocket(dai.CameraBoardSocket.LEFT)
+    else:
+        mono.setBoardSocket(dai.CameraBoardSocket.RIGHT)
+    return mono
 
-# Linking
-camRgb.video.link(xoutVideo.input)
 
-with dai.Device(pipeline) as device:
-    video = device.getOutputQueue(name="video", maxSize=1, blocking=False)
-    img_count = 0
-    frames = []
-    while True:
-        videoIn = video.get()
-        output = videoIn.getCvFrame()
-        # Get BGR from NV12 encoded video frame to show with opencv
-        cv2.imshow("video", output)
+def getStereoPair(pipeline, monoLeft, monoRight):
+    # Configure stereo pair for the depth estimation
+    stereo = pipeline.createStereoDepth()
+    # Checks occuluded pixels and marks them as invalid
+    stereo.setLeftRightCheck(True)
 
-        if cv2.waitKey(1) == ord('c'):
-            frames.append(output)
-            img_count += 1
-            print(len(frames))
-        if cv2.waitKey(1) == ord('p'):
-            print('Creating Panaroma')
-            if img_count < 2:
-                print('click more pictures for a panaroma')
-            else:
-                stitcher = cv2.Stitcher.create()
-                code, panaroma = stitcher.stitch(frames)
-                if code != cv2.STITCHER_OK:
-                    print("stitching not successful")
-                else:
-                    print('Your Panorama is ready!!!')
-                    cv2.imshow('final result', panaroma)
-                    cv2.imwrite('panaroma.jpg', panaroma)
+    # configure left and right cameras to work as a stereo pair
+    monoLeft.out.link(stereo.left)
+    monoRight.out.link(stereo.right)
 
-        if cv2.waitKey(1) == ord('q'):
-            break
+    return stereo
+
+
+def mouseCallback(event, x, y, flags, param):
+    global mouseX, mouseY
+    if event == cv2.EVENT_LBUTTONDOWN:
+        mouseX = x
+        mouseY = y
+
+
+if __name__ == '__main__':
+
+    mouseX = 0
+    mouseY = 640
+    # Start defining a pipeline
+    pipeline = dai.Pipeline()
+
+    # set up left and right cameras
+    monoLeft = getMonoCamera(pipeline, isLeft=True)
+    monoRight = getMonoCamera(pipeline, isLeft=False)
+    # combine left and right cameras to form a stereo pair
+    stereo = getStereoPair(pipeline, monoLeft, monoRight)
+    camRgb = pipeline.create(dai.node.ColorCamera)
+    camRgb.setBoardSocket(dai.CameraBoardSocket.RGB)
+    camRgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
+    camRgb.setVideoSize(1080, 720)
+    # Set XLinkOut for disparity, rectifiedLeft, and rectifiedRight
+    xoutDisp = pipeline.createXLinkOut()
+    xoutDisp.setStreamName("disparity")
+
+    xoutRectifiedLeft = pipeline.createXLinkOut()
+    xoutRectifiedLeft.setStreamName("rectifiedLeft")
+
+    xoutRectifiedRight = pipeline.createXLinkOut()
+    xoutRectifiedRight.setStreamName("rectifiedRight")
+
+    stereo.disparity.link(xoutDisp.input)
+
+    stereo.rectifiedLeft.link(xoutRectifiedLeft.input)
+
+    stereo.rectifiedRight.link(xoutRectifiedRight.input)
+    xoutRgb = pipeline.create(dai.node.XLinkOut)
+    xoutRgb.setStreamName("color")
+    camRgb.video.link(xoutRgb.input)
+    xoutRgb.input.setBlocking(False)
+    xoutRgb.input.setQueueSize(1)
+    # Pipeline is now defined, now we can connect to the device
+    with dai.Device(pipeline) as device:
+
+        # Output queues will be used to get the rgb frames and nn data from the outputs defined above
+        disparityQueue = device.getOutputQueue(name="disparity", maxSize=1, blocking=False)
+        rectifiedLeftQueue = device.getOutputQueue(name="rectifiedLeft", maxSize=1, blocking=False)
+        rectifiedRightQueue = device.getOutputQueue(name="rectifiedRight", maxSize=1, blocking=False)
+        video = device.getOutputQueue(name="color", maxSize=1, blocking=False)
+
+        # Calculate a multiplier for colormapping disparity map
+        disparityMultiplier = 255 / stereo.getMaxDisparity()
+
+        # Variable use to toggle between side by side view and one frame view.
+        sideBySide = False
+
+        while True:
+
+            # Get disparity map
+            disparity = getFrame(disparityQueue)
+            videoIn = getFrame(video)
+
+            # Colormap disparity for display
+            disparity = (disparity * disparityMultiplier).astype(np.uint8)
+            disparity = cv2.applyColorMap(disparity, cv2.COLORMAP_JET)
+            # Get left and right rectified frame
+            leftFrame = getFrame(rectifiedLeftQueue)
+            rightFrame = getFrame(rectifiedRightQueue)
+
+            cv2.imshow("Disparity", disparity)
+            cv2.imshow("RGB", videoIn)
+            # Check for keyboard input
+            key = cv2.waitKey(1)
+            if key == ord('q'):
+                # Quit when q is pressed
+                break
+            elif key == ord('t'):
+                # Toggle display when t is pressed
+                sideBySide = not sideBySide
